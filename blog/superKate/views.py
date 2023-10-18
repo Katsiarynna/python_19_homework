8d2819cf05354ca8b67bcff19eb045319656873d
Katsiarynna <113954361+Katsiarynna@users.noreply.github.com>
2023-10-18 14:12:10 +0200

    project

--- blog/superKate/views.py
+++ blog/superKate/views.py
@@ -1,6 +1,7 @@
+
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Page, Review
+from .models import Page, Review, Post, Posts
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
@@ -14,21 +15,33 @@ def page_view(request):
     context["all_pages"] = Page.objects.all()
     return render(request, "all_pages_func.html", context)
 
-
-
 def review_view(request):
     context = {}
     context["all_reviews"] = Review.objects.all()
     return render(request, "all_preview_func.html", context)
+
+#$$$$$$$$$$$$$$****************$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 class PageListView(ListView):
     model = Page
     template_name = "all_pages_class.html"
 
 
+class PostListView(ListView):
+    model = Post
+    template_name = "all_post_class.html"
+
+
+class PostsListView(ListView):
+    model = Posts
+    template_name = "all_posts_class.html"
+
+
 class ReviewListView(ListView):
     model = Review
     template_name = "all_review_class.html"
+
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$###################
 
 class PageCreateView(CreateView):
     model = Page
@@ -36,6 +49,17 @@ class PageCreateView(CreateView):
     fields = "__all__"
     success_url = "/superKate/all_pages_class/"
 
+class PostCreateView(CreateView):
+    model = Post
+    template_name = "create_post.html"
+    fields = "__all__"
+    success_url = "/superKate/all_pages_class/"
+
+class PostsCreateView(CreateView):
+    model = Posts
+    template_name = "create_posts.html"
+    fields = "__all__"
+    success_url = "/superKate/all_pages_class/"
 
 class ReviewCreateView(CreateView):
     model = Review
@@ -43,16 +67,26 @@ class ReviewCreateView(CreateView):
     fields = "__all__"
     success_url = "/superKate/all_review_class/"
 
+#$$$$$$$$$$$$$$((((((((((((((((((((&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
 
 class PageDetailView(DetailView):
     model = Page
     template_name = "detail_page_class.html"
 
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "detail_post_class.html"
+
+class PostsDetailView(DetailView):
+    model = Posts
+    template_name = "detail_posts_class.html"
 
 class ReviewDetailView(DetailView):
     model = Review
     template_name = "detail_review_class.html"
 
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 class PageUpdateView(UpdateView):
     model = Page
@@ -60,7 +94,17 @@ class PageUpdateView(UpdateView):
     template_name = "update_page.html"
     success_url = "/superKate/all_pages_class/"
 
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ("title", "description")
+    template_name = "update_post.html"
+    success_url = "/superKate/all_pages_class/"
 
+class PostsUpdateView(UpdateView):
+    model = Posts
+    fields = ("title", "description")
+    template_name = "update_posts.html"
+    success_url = "/superKate/all_pages_class/"
 
 class ReviewUpdateView(UpdateView):
     model = Review
@@ -68,18 +112,28 @@ class ReviewUpdateView(UpdateView):
     template_name = "update_review.html"
     success_url = "/superKate/all_review_class/"
 
-
+#!!!!!!!!!!!!!!****************!!!!!!!!!!!!!!!!!!!*************
 class PageDeleteView(DeleteView):
     model = Page
     template_name = "delete_view.html"
     success_url = "/superKate/all_pages_class/"
 
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = "delete_view.html"
+    success_url = "/superKate/all_pages_class/"
 
+class PostsDeleteView(DeleteView):
+    model = Posts
+    template_name = "delete_view.html"
+    success_url = "/superKate/all_pages_class/"
 
 class ReviewDeleteView(DeleteView):
     model = Review
     template_name = "delete_view.html"
     success_url = "/superKate/all_review_class/"
+
+
 
 
 
